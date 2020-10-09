199ca31813514e5ab675a8dc68029732574016ad
bapk <BlessingApkarian@github.com>
2020-10-08 20:38:16 -0700

    space invader has boundy, slowed movement, bullets colliding with enemies destroyes enemies and bullet + particle effect (explosion)

---
+++ SpaceInvader/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject particleEffect;
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ouch!");
+        // destroy bullet
+        Destroy(collision.gameObject);
+        // destroy enemy
+        Destroy(gameObject);
+        // start particle explosion
+        GameObject particle = Instantiate(particleEffect, transform.position, Quaternion.identity);
+        // end explosion after 2 seconds
+        Destroy(particle, 2f);
+    }
+}
---
+++ SpaceInvader/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform player;
+
+    public GameObject bullet;
+
+    private float velocityPlayer = 0;
+
+    public Transform shottingOffset;
+
+    [SerializeField] private float amplitude = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocityPlayer = Input.GetAxis("Horizontal");
+        Debug.Log(velocityPlayer);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+            Debug.Log("Bang!");
+
+            Destroy(shot, 3f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer(new Vector3(velocityPlayer, 0, 0), player);
+    }
+
+    void MovePlayer(Vector3 direction, Transform player)
+    {
+        player.Translate(direction * amplitude);
+    }
+}
