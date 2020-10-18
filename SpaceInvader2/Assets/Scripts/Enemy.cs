2c32372b6283f2046b5bc5378d4248c7563a932f
bapk <BlessingApkarian@github.com>
2020-10-17 20:19:24 -0700

    switching scenes works

---
+++ SpaceInvader2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      GetComponent<Animator>().SetTrigger("Death");
+      Destroy(collision.gameObject); // destroy bullet
+      Destroy(gameObject, 1f); // destroy enemy (after 1 second so we can see the death animation)
+      Debug.Log("Ouch!");
+    }
+}
