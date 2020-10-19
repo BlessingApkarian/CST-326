3b43302be36f46e527a8a6730d6df0aad8ed9961
bapk <BlessingApkarian@github.com>
2020-10-18 21:24:33 -0700

    switch from game to credits when player dies, switch from credits to menu after 5 seconds

--- SpaceInvader2/Assets/Scripts/SceneSwitcher.cs
+++ SpaceInvader2/Assets/Scripts/SceneSwitcher.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
+    private float timer = 5;
+    private float timeElapsed = 0;
+    public string sceneName = "";
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > timer)
+        {
+            Debug.Log(timeElapsed);
+            resetGame();
+            timer = 0;
+        }
+    }
+
     public void playGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void resetGame()
+    {
+        if (sceneName == "Credits")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
 }
