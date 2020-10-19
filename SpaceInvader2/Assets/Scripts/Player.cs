be5e1d240b6b1ca18609e8e84fe8752b64da7afe
bapk <BlessingApkarian@github.com>
2020-10-18 22:27:45 -0700

    added player idle, shooting and death animation

--- SpaceInvader2/Assets/Scripts/Player.cs
+++ SpaceInvader2/Assets/Scripts/Player.cs
@@ -10,25 +10,29 @@ public class Player : MonoBehaviour
 
   public Transform shoottingOffset;
 
-  void Start()
-  {
-    playerAnimator = GetComponent<Animator>();
-  }
+    private float timer = 5;
+    private float timeElapsed = 0;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space))
-      {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
 
-        playerAnimator.SetTrigger("Shoot");
-        GameObject shot = Instantiate(bullet, shoottingOffset.position, Quaternion.identity);
-        Debug.Log("Bang!");
+            playerAnimator.SetTrigger("Shoot");
+            GameObject shot = Instantiate(bullet, shoottingOffset.position, Quaternion.identity);
+            Debug.Log("Bang!");
 
-        Destroy(shot, 3f);
+            Destroy(shot, 3f);
 
-      }
-      // TODO: if shot, go to credits
+        }
+        // TODO: if shot, go to credits
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -42,8 +46,20 @@ public class Player : MonoBehaviour
             GetComponent<Animator>().SetTrigger("Death");
             Destroy(collision.gameObject); // destroy bullet
             Destroy(gameObject, 1f); // destroy enemy (after 1 second so we can see the death animation)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // go to end credits when player dies
+            Debug.Log("Dead");
+            goToCredits();
         }
 
+    }
+
+    private void goToCredits()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timer < timeElapsed)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // go to end credits when player dies
+            timeElapsed = 0;
+        }
     }
 }
