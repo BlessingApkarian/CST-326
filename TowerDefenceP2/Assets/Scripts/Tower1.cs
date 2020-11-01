a7bcbb1f9dc9edd0190adefa2b1960632c98b9be
bapk <BlessingApkarian@github.com>
2020-11-01 11:48:21 -0800

    tower defense part 2 finished

---
+++ TowerDefenceP2/Assets/Scripts/Tower1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower1 : MonoBehaviour
+{
+    public Transform tower;
+    public Transform towerParent;
+
+    void placeTower()
+    {
+        
+        {
+
+            //Instantiate(tower, hit.transform.position, Quaternion.identity, towerParent);
+            //Destroy(hit.transform.gameObject);
+
+        }
+    }
+}
