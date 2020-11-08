4ec33e1f2d67848ceec55274e59b1387f009a106
bapk <BlessingApkarian@github.com>
2020-11-08 11:17:22 -0800

    tower defense 3, enemies have separate paths

---
+++ TowerDefenseP3/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Group
+{
+  public GameObject enemyType;
+  [Range(0,50)] public int amountOfEnemies;
+  [Range(0,5)] public float timeDelayBetweenEnemies;
+
+  public Group(GameObject enemyType, int amountOfEnemies, float timeDelayBetweenEnemies)
+  {
+    this.enemyType = enemyType;
+    this.amountOfEnemies = amountOfEnemies;
+    this.timeDelayBetweenEnemies = timeDelayBetweenEnemies;
+  }
+}
+
+[System.Serializable]
+public struct Wave
+{
+  public Group[] enemyGroups;
+}
+
+
+public class EnemyManager : MonoBehaviour
+{
+ //Figure out what enemy waves look like and how to handle them (e.g. spawning, how many, wait time in between)
+ 
+
+ //Pass navigational path
+ public Waypoints[] topNavPoints;
+ public Waypoints[] botNavPoints;
+ public Wave enemyWave;
+ //public GameObject easyEnemy;
+ //public GameObject hardEnemy;
+
+
+ void Start()
+ {
+   SpawnWave();
+ }
+
+ private void SpawnWave()
+ {
+
+   foreach (Group group in enemyWave.enemyGroups)
+   {
+     StartCoroutine(SpawnGroup(group));
+     
+   }
+ }
+
+ IEnumerator SpawnGroup(Group enemyGroup)
+ {
+   int i = 0;
+   while (enemyGroup.amountOfEnemies > 0)
+   {
+     GameObject spawnedEnemy = Instantiate(enemyGroup.enemyType);
+            Debug.Log(spawnedEnemy.name);
+            if(spawnedEnemy.name.Contains("EasyEnemy"))
+            {
+                Debug.Log("easy");
+
+                spawnedEnemy.GetComponent<Enemy>().StartEnemy(topNavPoints);
+            } else if (spawnedEnemy.name.Contains("HardEnemy Variant"))
+            {
+                Debug.Log("hard");
+
+                spawnedEnemy.GetComponent<Enemy>().StartEnemy(botNavPoints);
+            }
+     
+     spawnedEnemy.name = $"{enemyGroup.enemyType.ToString()} {i}";
+     enemyGroup.amountOfEnemies--;
+     i++;
+     yield return new WaitForSeconds(enemyGroup.timeDelayBetweenEnemies);
+     
+   }
+   
+  }
+}
---
+++ TowerDefenseP3/Assets/Scripts/Tower.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tower : MonoBehaviour
+{
+  public List<Enemy> currentEnemies;
+  public Enemy currentTarget;
+
+  public GameObject turret;
+  private LineRenderer lineRenderer; 
+
+  void Start()
+  {
+    lineRenderer = GetComponent<LineRenderer>();
+  }
+
+  void OnDrawGizmos()
+  {
+    Gizmos.DrawWireSphere(transform.position, GetComponent<SphereCollider>().radius); // draw circle around tower
+  }
+
+  void Update()
+  {
+    if (currentTarget)
+    {
+      lineRenderer.SetPosition(0, turret.transform.position); // draw line beteween tower and current enemy target 
+      lineRenderer.SetPosition(1, currentTarget.transform.position);
+    }
+  }
+
+  void OnTriggerEnter(Collider collider) // enemy enters sphere (tower hit radius)
+  {
+    Enemy newEnemy = collider.GetComponent<Enemy>(); // enemy that enters sphere is a new enemy
+    currentEnemies.Add(newEnemy); // new enemy gets added to enemies list
+
+    newEnemy.DeathEvent.AddListener(delegate { BookKeeping(newEnemy);}); // subscribe to death event, when the enemy dies, tower needs to know
+
+    EvaluateTarget(newEnemy);
+
+    //Debug.Log($"{collider.name} has entered");
+  }
+
+  void OnTriggerExit(Collider collider)
+  {
+    Enemy enemyLeaving = collider.GetComponent<Enemy>();
+
+    //enemyLeaving.DeathEvent.RemoveListener(delegate { BookKeeping(enemyLeaving);}); //unsubscribing to the DeathEvent for this enemy .... don't care anymore :(
+
+    currentEnemies.Remove(enemyLeaving);  // enemy leaving the circle, remove from enemy list
+    EvaluateTarget(enemyLeaving);
+
+  }
+
+  private void BookKeeping(Enemy enemy)
+  {
+    currentEnemies.Remove(enemy);
+    EvaluateTarget(enemy);
+
+  }
+
+
+  private void EvaluateTarget(Enemy enemy)// if the enemy the tower is currently targeting is the enemy being evaluated 
+  {
+    if (currentTarget == enemy) // if enemy target is leaving sphere, reset currentTarget value
+    {
+      currentTarget = null;
+      lineRenderer.enabled = false;
+    }
+
+    if (currentTarget == null) // if no enemy is being targeted, set first enemy in list (first to enter the sphere) to current target
+    {
+      currentTarget = currentEnemies[0];
+      lineRenderer.enabled = true;
+    }
+  }
+}
