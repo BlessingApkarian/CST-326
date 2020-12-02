92b2da520d9eb5c7e67ce5ee68417ceee196965c
bapk <BlessingApkarian@github.com>
2020-12-01 21:08:41 -0800

    Final Project, Player interacts with ground properly & movement finished

--- Final_Game_Project/Assets/Scripts/PlayerMovement.cs
+++ Final_Game_Project/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller; // motor that drives player
     public Transform cam;
 
+
+    public float gravity = 9.8f;
     public float speed = 1f;
+    public float jumpSpeed = 5f;
+    public float verticalSpeed = 0f;
 
     public float smoothTurnTime = 0.1f;
     public float turnSmoothVel;
@@ -21,14 +25,37 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // move x & z, normalize makes us able to move diagonal
 
-        if(direction.magnitude >= 0.1f) // keys are being pressed
+        
+
+        if (direction.magnitude >= 0.1f) // keys are being pressed
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // atan2 method calculates angle between the x-axis and the vector starting at 0 (look at a graph with a line) 
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, smoothTurnTime); // smooth player turning
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            //controller.Move(direction * speed * Time.deltaTime);
+
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            if (controller.isGrounded)
+            {
+                verticalSpeed = 0;
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    verticalSpeed = jumpSpeed;
+                }
+
+            }
+            else
+            {
+                verticalSpeed -= gravity * Time.deltaTime;
+            }
+
+            direction.y = verticalSpeed;
+
+            controller.Move(direction * Time.deltaTime);
         }
     }
 }
