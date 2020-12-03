03765ac0423f368207afd5dd425da1a885e412f1
bapk <BlessingApkarian@github.com>
2020-12-03 11:47:36 -0800

    raycast implemented, temp NPC, enemy & collectable

--- Final_Game_Project/Assets/Scripts/ClickNPC.cs
+++ Final_Game_Project/Assets/Scripts/ClickNPC.cs
@@ -7,7 +7,7 @@ public class ClickNPC : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // left mouse button
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -17,9 +17,46 @@ public class ClickNPC : MonoBehaviour
                 if (hit.transform)
                 {
                     Debug.Log(hit.transform.name);
-                    if (hit.transform.tag.Equals("NPC"))
+                    if (hit.transform.CompareTag("NPC"))
+                    {
+                        Debug.Log("right click to speak to NPC");
+                    }
+
+                    if (hit.transform.CompareTag("hitable"))
+                    {
+                        Debug.Log("right click to attack");
+                    }
+
+                    if (hit.transform.CompareTag("collectable"))
+                    {
+                        Debug.Log("right click to gather items");
+                    }
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1)) // right mouse button (middle is 2)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    Debug.Log(hit.transform.name);
+                    if (hit.transform.CompareTag("NPC"))
                     {
                         OpenDialog();
+                    }
+
+                    if (hit.transform.CompareTag("hitable"))
+                    {
+                        DoDamage();
+                    }
+
+                    if (hit.transform.CompareTag("collectable"))
+                    {
+                        GatherItems();
                     }
                 }
             }
@@ -29,6 +66,16 @@ public class ClickNPC : MonoBehaviour
     void OpenDialog()
     {
         Debug.Log("Hello Izabelle");
+    }
+
+    void DoDamage()
+    {
+        Debug.Log("Damage Done: ");
+    }
+
+    void GatherItems()
+    {
+        Debug.Log("Gathered: ");
     }
 
 }
